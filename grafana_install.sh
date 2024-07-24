d19511cae2ac14790eaef22a6a007b23873d6a55
user <vasechkin.alex.v@gmail.com>
2024-07-24 20:00:28 +0300

    grafana ingress

--- grafana_install.sh
+++ grafana_install.sh
@@ -7,3 +7,5 @@ tar -zxf grafana-*.tgz
 cp grafana/values.yaml grafana-values.yaml
 
 helm upgrade --install --create-namespace --values grafana-values.yaml grafana -n loki grafana/grafana
+
+kubectl get secret --namespace loki grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
